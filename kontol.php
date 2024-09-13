16d148b15811e2edf426313ded1e2fd63f0af7dc
regina0712 <159528728+regina0712@users.noreply.github.com>
2024-09-14 06:59:42 +0700

    Create kontol.php
    pengullangan

---
+++ kontol.php
@@ -0,0 +1,11 @@
+} echo "CONTOH FOR <br>";
+for($i=1;$i<=5;$i++) {
+echo "Looping FOR ke : ".$i."<br>";
+}
+echo "CONTOH WHILE <br>";
+$j=1;
+while($j<=5) {
+echo "Looping While ke : ".$j."<br>";
+$j++;
+}
+?>
