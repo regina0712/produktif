30fc3b18ff13ffe1961a513c2d3b7b362830b585
regina0712 <159528728+regina0712@users.noreply.github.com>
2024-09-14 06:58:27 +0700

    Create kontrol_ulang.php
    perulangan

---
+++ kontrol_ulang.php
@@ -0,0 +1,11 @@
+<?php
+$nilai=90;
+echo "CONTOH IF ELSE <br>";
+if($nilai>80) { echo "Selamat Anda mendapat grade A <br>";}
+else { echo "Maaf Anda belum dapat grade A <br>";};
+echo "CONTOH SWITCH <br>";
+switch($nilai) {
+case 100 :echo "Nilai yang dipilih 100 <br>";
+break;
+case 90 :echo "Nilai yang dipilih 90 <br>";
+break;
