3d1694fe101a6de09551513da0b238ec2bccc7aa
regina0712 <159528728+regina0712@users.noreply.github.com>
2024-09-14 07:10:25 +0700

    Update kontrol_ulang.php

--- kontrol_ulang.php
+++ kontrol_ulang.php
@@ -1,11 +1,11 @@
 <?php
-$nilai=90;
+$nilai=100;
 echo "CONTOH IF ELSE <br>";
-if($nilai>80) { echo "Selamat Anda mendapat grade A <br>";}
+if($nilai>1000) { echo "Selamat Anda mendapat grade A <br>";}
 else { echo "Maaf Anda belum dapat grade A <br>";};
 echo "CONTOH SWITCH <br>";
 switch($nilai) {
 case 100 :echo "Nilai yang dipilih 100 <br>";
 break;
-case 90 :echo "Nilai yang dipilih 90 <br>";
+case 1000 :echo "Nilai yang dipilih 1000 <br>";
 break;
